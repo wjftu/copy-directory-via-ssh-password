10b033e52c780b69d31e1d05bf752da463f5035a
hadoop <>
2023-12-12 21:06:46 +0800

    bugfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sshpass -p "${3}" ssh ${2}@${1} "mkdir -p ${5}" 
-sshpass -p "${3}" scp -r ${4} StrictHostKeyChecking=no ${2}@${1}:${5}
+sshpass -p "${3}" ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${2}@${1} "mkdir -p ${5}" 
+sshpass -p "${3}" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r ${4} ${2}@${1}:${5}
